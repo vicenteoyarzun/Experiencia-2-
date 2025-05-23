e7a17281ef0a24239da4b78817e42ed48b344198
vicenteoyarzun <163044338+vicenteoyarzun@users.noreply.github.com>
2025-05-23 13:03:32 -0400

    Add files via upload

---
+++ Proyecto perfulandia-mish/perfulandia-backend-01V-master/AutenticacionService/AutenticacionService/src/test/java/com/perfulandia/AutenticacionService/AutenticacionService/AutenticacionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.AutenticacionService.AutenticacionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
