8c60add52fb66d93dad635c039b2b0d593edd701
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2025-05-27 13:58:17 -0400

    Inicio Microservicio autenticacion

--- Proyecto perfulandia-mish/perfulandia-backend-01V-master/AutenticacionService/AutenticacionService/src/main/java/com/perfulandia/AutenticacionService/AutenticacionService/service/AutenticationService.java
+++ Proyecto perfulandia-mish/perfulandia-backend-01V-master/AutenticacionService/AutenticacionService/src/main/java/com/perfulandia/AutenticacionService/AutenticacionService/service/AutenticationService.java
@@ -1,4 +1,6 @@
 package com.perfulandia.AutenticacionService.AutenticacionService.service;
-
+import com.perfulandia.AutenticacionService.AutenticacionService.model.Usuario;
+import java.util.List;
+import com.perfulandia.AutenticacionService.AutenticacionService.repository.AutenticacionRepository;
 public class AutenticationService {
 }
